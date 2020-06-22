99c9f54d4770533a17c13c725803f2b0ef38cacf
Saleh Alibrahim <s.albrahim@outlook.com>
2020-06-22 20:16:19 +0300

    deploy for heroku

--- public/javascripts/res.js
+++ public/javascripts/res.js
@@ -1,44 +1,21 @@
 //Get the main form
-let vote_button = document.getElementById('submit-vote');
-vote_button.addEventListener('click', async (e) => {
+let copy_button = document.getElementById('copy');
+copy_button.addEventListener('click', async (e) => {
     try {
 
-        //get the selected option of the poll
-        let selected_option = document.querySelector('input[name="itemList"]:checked');
+        //get the res url
+        let res_url = window.location.href;
 
-        //show the alert of select  requierd
-        let select_alert = document.getElementById('select-alert');
-        if (!selected_option) {
-            //show the alert and then return
-            if (select_alert.classList.contains('hidden'))
-                select_alert.classList.remove('hidden');
-            return;
-        } else {
-            //hide the alert and then continue
-            if (!select_alert.classList.contains('hidden'))
-                select_alert.classList.add('hidden');
-        }
+        //remove the /res from the end
+        let url = res_url.slice(0, res_url.length - 4);
 
-        //get the selected vote id
-        let id = selected_option.id;
-
-        // add vote 
-        let response = await fetch('/add_vote ', {
-            method: 'POST',
-            body: JSON.stringify({
-                'id': id,
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        let response_data = await response.json();
-
-        //render the new html page if resopne success
-        if (response_data.success)
-            window.location.href = window.location.href + `/res`;
-
+        //source https://stackoverflow.com/questions/49618618/copy-current-url-to-clipboard
+        const dummy = document.createElement('input');
+        document.body.appendChild(dummy);
+        dummy.value = url;
+        dummy.select();
+        document.execCommand('copy');
+        document.body.removeChild(dummy);
     }
     catch (error) {
         console.log(error);
