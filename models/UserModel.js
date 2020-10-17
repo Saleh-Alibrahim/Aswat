48d725343dbdda3c34e24ad22259a99029483d45
Saleh Alibrahim <s.albrahim@outlook.com>
2020-10-17 23:15:19 +0300

    fix: fix error in displaying user roles

--- models/UserModel.js
+++ models/UserModel.js
@@ -25,8 +25,8 @@ const UserSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['مستخدم', 'مدير'],
-        default: 'مستخدم'
+        enum: ['مستخدم اصوات', 'مدير اصوات'],
+        default: 'مستخدم اصوات'
     },
     password: {
         type: String,
